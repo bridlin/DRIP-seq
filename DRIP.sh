26ff0e525410d5da75b9b4cd948de32f2f651d1e
bridlin <bridlinbarckmann@gmail.com>
2025-03-19 13:36:07 +0100

    sample change

--- DRIP.sh
+++ DRIP.sh
@@ -18,9 +18,9 @@ module load samtools/1.13
 module load trim-galore/0.6.5
 module load deeptools/3.5.0
 
-input_list=("ERR2071904" "ERR2071905" "ERR2071908" "ERR2071909")
-input_dir=fastq_DRIP_RNAi/
-output_dir=Drip-seq_analysis
+input_list=("ERR2037037_" "ERR2037038_")
+input_dir=DRIP_fastq/
+output_dir=Drip-seq_analysis_2018_H1mut
 genome=genome/TriTrypDB-55_TbruceiLister427_2018_Genome/index_bowtie2/TriTrypDB-55_TbruceiLister427_2018_Genome
 # genome=genome/TriTrypDB-62_TbruceiTREU927_Genome/index_bowtie2/TriTrypDB-62_TbruceiTREU927_Genome
 prefix=aln_Tb427_2018_v55
@@ -28,25 +28,26 @@ prefix=aln_Tb427_2018_v55
 
 mkdir $output_dir
 for x in "${input_list[@]}"; do
-# trim_galore \
-# 	--paired \
-# 	--output_dir $input_dir/ $input_dir/$x\_1.fastq.gz  $input_dir/$x\_2.fastq.gz && 
-# # # Briggs 2018 used the -very-sensitive option of bowtie2
-# bowtie2 \
-# 	--threads 6 \
-# 	-x $genome \
-# 	-1 $input_dir/$x\_1_val_1.fq.gz -2 $input_dir/$x\_2_val_2.fq.gz   \
-# 	-S $output_dir/$x\_$prefix\.sam &&
-# samtools view  -b -q 20 $output_dir/$x\_$prefix\.sam > $output_dir/$x\_$prefix\_filtered.bam &&
-# samtools sort $output_dir/$x\_$prefix\_filtered.bam -o $output_dir/$x\_$prefix\_filtered_sorted.bam &&
-# rm -f  $output_dir/$x\_$prefix\.sam &&
-# rm -f  $output_dir/$x\_$prefix\_filtered.bam  &&
+trim_galore \
+	--paired \
+	--output_dir $input_dir/ $input_dir/$x\_1.fastq.gz  $input_dir/$x\_2.fastq.gz && 
+# # Briggs 2018 used the -very-sensitive option of bowtie2
+bowtie2 \
+	--threads 6 \
+	-x $genome \
+	--very-sensitive \
+	-1 $input_dir/$x\_1_val_1.fq.gz -2 $input_dir/$x\_2_val_2.fq.gz   \
+	-S $output_dir/$x\_$prefix\.sam &&
+samtools view  -b -q 20 $output_dir/$x\_$prefix\.sam > $output_dir/$x\_$prefix\_filtered.bam &&
+samtools sort $output_dir/$x\_$prefix\_filtered.bam -o $output_dir/$x\_$prefix\_filtered_sorted.bam &&
+rm -f  $output_dir/$x\_$prefix\.sam &&
+rm -f  $output_dir/$x\_$prefix\_filtered.bam  &&
 samtools index $output_dir/$x\_$prefix\_filtered_sorted.bam ; done
 
-input=("ERR2071904" "ERR2071908")
-sample=("ERR2071905" "ERR2071909")
+input=("ERR2037038_")
+sample=("ERR2037037_")
 
-for y in 0 1 2 ; do
+for y in 0; do
 	echo ${sample[$y]}
 	echo ${input[$y]}
 	bamCompare  \
